947efe1e1c6946a6968f61980e91be2130413a69
ghost <brennan1086@hotmai.com>
2020-08-25 13:17:00 -0600

    v1 of dynamically inserting students

--- js/script.js
+++ js/script.js
@@ -20,20 +20,37 @@ This function will create and insert/append the elements needed to display a "pa
 const page = 1;
 const itemsPerPage = 9;
 
-function showPage(list, page){
+function showPage(pageNumber, studentsPerPage){
+
+   const list = document.getElementById('student-list');
    
-   let startIndex = itemsPerPage * page;
-   let endIndex = startIndex + itemsPerPage;
-   let studentList = document.getElementsByClassName("student-list").value;
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   let studentList = document.getElementsByClassName("student-list");
    studentList.innerHTML = '';
 
    for (let i = 0; i < list.length; i++){
       if(i >= startIndex && i < endIndex){
-         const li = document.createElement('LI');
-         const div = document.createElement('DIV');
-         const img = document.createElement('IMG');
-         const h3 = document.createElement('H3');
-         const span = document.createElement('SPAN');
+         let li = document.createElement('LI');
+         let div = document.createElement('DIV');
+         let img = document.createElement('IMG');
+         let h3 = document.createElement('H3');
+         let span= document.createElement('SPAN');
+
+         list.appendChild(li);
+         li.className = 'student-item';
+         li.appendChild(div);
+         div.className = 'student-details';
+         div.appendChild(img)
+         img.className = 'avatar';
+         div.appendChild(h3);
+         div.appendChild(span);
+         span.className = 'email';
+         li.appendChild(div);
+         div.className = 'joined-details';
+         div.appendChild(span);
+         span.className = 'date';
+         
       }
    }
 };
