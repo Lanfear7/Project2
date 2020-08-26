8f09e1067bfdbce1c1b0282151575f1e5475a35e
ghost <brennan1086@hotmai.com>
2020-08-25 21:42:45 -0600

    moved code around

--- js/script.js
+++ js/script.js
@@ -26,31 +26,19 @@ function showPage(pageNumber, studentsPerPage){
    
    let startIndex = (page * itemsPerPage) - itemsPerPage;
    let endIndex = page * itemsPerPage;
+   
    let studentList = document.getElementsByClassName("student-list");
    studentList.innerHTML = '';
 
+   let li = document.createElement('LI');
+   let div = document.createElement('DIV');
+   let img = document.createElement('IMG');
+   let h3 = document.createElement('H3');
+   let span= document.createElement('SPAN');
+
    for (let i = 0; i < list.length; i++){
       if(i >= startIndex && i < endIndex){
-         let li = document.createElement('LI');
-         let div = document.createElement('DIV');
-         let img = document.createElement('IMG');
-         let h3 = document.createElement('H3');
-         let span= document.createElement('SPAN');
-
-         list.appendChild(li);
-         li.className = 'student-item';
-         li.appendChild(div);
-         div.className = 'student-details';
-         div.appendChild(img)
-         img.className = 'avatar';
-         div.appendChild(h3);
-         div.appendChild(span);
-         span.className = 'email';
-         li.appendChild(div);
-         div.className = 'joined-details';
-         div.appendChild(span);
-         span.className = 'date';
-         
+ 
       }
    }
 };
